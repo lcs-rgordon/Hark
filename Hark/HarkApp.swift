cee96423bfef931ae046cb5b5a4e2a488f4c12b0
Russell Gordon <rgordon@lcs.on.ca>
2024-06-03 15:01:36 -0400

    Initial Commit

---
+++ Hark/HarkApp.swift
@@ -0,0 +1,17 @@
+//
+//  HarkApp.swift
+//  Hark
+//
+//  Created by Russell Gordon on 2024-06-03.
+//
+
+import SwiftUI
+
+@main
+struct HarkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
