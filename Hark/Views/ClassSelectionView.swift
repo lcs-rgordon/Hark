3f6837f9800410af9823df0386ea322ec2765e37
Russell Gordon <rgordon@lcs.on.ca>
2024-06-03 15:28:40 -0400

    Got navigation to a second view working.

---
+++ Hark/Views/ClassSelectionView.swift
@@ -0,0 +1,18 @@
+//
+//  ClassSelectionView.swift
+//  Hark
+//
+//  Created by Russell Gordon on 2024-06-03.
+//
+
+import SwiftUI
+
+struct ClassSelectionView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ClassSelectionView()
+}
--- Hark/Views/WelcomeView.swift
+++ Hark/Views/WelcomeView.swift
@@ -10,51 +10,54 @@ import SwiftUI
 struct WelcomeView: View {
     var body: some View {
         
-        ZStack {
-            
-            Color.welcomeScreenBlue
-                .ignoresSafeArea()
-            
-            VStack {
-                Spacer()
+        NavigationStack {
+            ZStack {
                 
-                Text("Hark")
-                    .foregroundStyle(Color.white)
-                    .font(.custom("EuphemiaUCAS-Bold", size: 96.0, relativeTo: .largeTitle))
-                    .shadow(color: .gray, radius: 10)
-
-                Text("""
-                    Welcome to Hark,
-                    a Harkness app which
-                    is designed to help
-                    you succeed during
-                    your Harkness sessions!
-                    """)
-                    .multilineTextAlignment(.center)
-                    .foregroundStyle(Color.white)
-                    .font(.custom("EuphemiaUCAS-Bold", size: 24.0, relativeTo: .largeTitle))
-
-                Spacer()
+                Color.welcomeScreenBlue
+                    .ignoresSafeArea()
                 
-                NavigationLink {
-                    Text("Second view")
-                } label: {
-                    Text("Go to Classes")
+                VStack {
+                    Spacer()
+                    
+                    Text("Hark")
                         .foregroundStyle(Color.white)
-                        .font(.custom("EuphemiaUCAS-Bold", size: 30.0, relativeTo: .largeTitle))
-                        .padding()
-                        .padding(.vertical, 10)
-                        .background {
-                            RoundedRectangle(cornerRadius: 20.0)
-                                .foregroundStyle(.gray)
-                        }
-                }
+                        .font(.custom("EuphemiaUCAS-Bold", size: 96.0, relativeTo: .largeTitle))
+                        .shadow(color: .gray, radius: 10)
 
+                    Text("""
+                        Welcome to Hark,
+                        a Harkness app which
+                        is designed to help
+                        you succeed during
+                        your Harkness sessions!
+                        """)
+                        .multilineTextAlignment(.center)
+                        .foregroundStyle(Color.white)
+                        .font(.custom("EuphemiaUCAS-Bold", size: 24.0, relativeTo: .largeTitle))
+
+                    Spacer()
+                    
+                    NavigationLink {
+                        Text("Second view")
+                    } label: {
+                        Text("Go to Classes")
+                            .foregroundStyle(Color.white)
+                            .font(.custom("EuphemiaUCAS-Bold", size: 30.0, relativeTo: .largeTitle))
+                            .padding()
+                            .padding(.vertical, 10)
+                            .background {
+                                RoundedRectangle(cornerRadius: 20.0)
+                                    .foregroundStyle(.gray)
+                            }
+                    }
+
+                    
+                    Spacer()
+                }
                 
-                Spacer()
             }
-            
         }
+        
     }
 }
 
